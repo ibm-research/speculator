86264895301d763233fc1ac288dcb3593e3fe6e3
Andrea Mambretti <amb@zurich.ibm.com>
2019-09-04 00:19:39 +0200

    Speculation window size - Store to load forwarding experiment

---
+++ examples/v4_cycles/v4_cycles.asm
@@ -0,0 +1,72 @@
+[BITS 64]
+    %include "common.inc"
+    %include "intel.inc"
+
+    section .data
+    warmup_cnt: db 1
+    fill: times 63 db 0
+
+    warmup_cnt_fake: dq 1 
+    fill2: times 60 db 0
+
+    array: times 64 db 0
+
+    dev_file: db '/dev/cpu/',VICTIM_PROCESS_STR,'/msr',0
+    fd: dq 0
+    offset: dq 0
+    val: dq 0
+    len: equ $-val
+    ;##### DATA STARTS HERE ########
+
+    ;##### DATA ENDS HERE ########
+
+    section .text
+    global perf_test_entry:function
+    global snippet:function
+
+perf_test_entry:
+    push rbp
+    mov rbp, rsp
+    sub rsp, len
+
+    check_pinning VICTIM_PROCESS
+    msr_open
+    msr_seek
+    lfence
+    pipeline_flush
+    reset_counter
+    start_counter
+    mov rsi, fill2
+    mov rdi, fill2
+    mov rax, rdi
+    xor rdx, rdx
+    mov rcx, 2
+    div rcx
+    mov rdi, rax
+    mov rax, 2
+    mov rcx, 10
+    mul rdi
+    mov rdi, rax
+    mov rdx, fill
+    mov al, 0x10
+    mov [rdi+rcx], al
+    movzx r8, byte[rsi+rcx]
+    shl r8, byte 0x1
+    ;##### SNIPPET STARTS HERE ######
+
+    ;##### SNIPPET ENDS HERE ######
+
+    ; UNCOMMENT TO TEST CYCLES span for speculation
+
+    mov QWORD[warmup_cnt_fake], rdx
+    mov DWORD[warmup_cnt_fake + 4], edx
+    mov rax, QWORD[warmup_cnt_fake]
+
+    ; OR
+
+    ;mulps xmm0,xmm1
+
+    stop_counter
+
+    msr_close
+    exit 0
