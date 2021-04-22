211e06b51f1314f6fb63a3e7ccc8ce3df3188c42
Andrea Mambretti <amb@zurich.ibm.com>
2021-04-22 14:05:43 +0200

    Changed copyright year

--- examples/SPEAR/link_spear.zsh
+++ examples/SPEAR/link_spear.zsh
@@ -1,5 +1,5 @@
 #!/bin/zsh
-# Copyright 2019 IBM Corporation
+# Copyright 2021 IBM Corporation
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
--- examples/mpx/mpx.asm
+++ examples/mpx/mpx.asm
@@ -1,4 +1,4 @@
-i; Copyright 2019 IBM Corporation
+i; Copyright 2021 IBM Corporation
 ;
 ; Licensed under the Apache License, Version 2.0 (the "License");
 ; you may not use this file except in compliance with the License.
